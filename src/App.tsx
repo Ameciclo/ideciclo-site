7782fc8aba6f2408514c10a1a3da56a46f4f865d
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2025-08-11 14:22:49 -0300

    scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CityDetails from "./pages/CityDetails";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -49,6 +50,7 @@ const App = () => (
             </Routes>
           </div>
           <Footer />
+          <ScrollToTop />
         </div>
       </BrowserRouter>
     </TooltipProvider>
