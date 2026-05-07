b312935a3c1c899b2b8c5c8781ded988a8ca60d6
Daniel Valença <dvalenca@gmail.com>
2026-05-06 22:52:25 -0300

    .

---
+++ api/auth/admin/permissions/[permissionId].js
@@ -0,0 +1,5 @@
+import { handleAuthRequest } from "../../../../server/auth-server.mjs";
+
+export default async function handler(request, response) {
+  return handleAuthRequest(request, response);
+}
